305b2a86324fa73b8f14f927e92d245a0eb7d19d
Luiz Augusto <luiz.santiago@radixeng.com.br>
2020-09-04 18:07:14 -0300

    Subindo projeto

---
+++ firebase-cloud-messaging/public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts ('https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js')
+importScripts ('https://www.gstatic.com/firebasejs/7.19.1/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/7.19.1/firebase-analytics.js')
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+    }
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDQOtJLOTNnD0p03dbNPyLKttoPhISUoqM",
+    authDomain: "fir-cloud-messaging-379af.firebaseapp.com",
+    databaseURL: "https://fir-cloud-messaging-379af.firebaseio.com",
+    projectId: "fir-cloud-messaging-379af",
+    storageBucket: "fir-cloud-messaging-379af.appspot.com",
+    messagingSenderId: "1081227225213",
+    appId: "1:1081227225213:web:1afe53577d982bbd6b2914",
+    measurementId: "G-1C3M64FG6L"
+  })
+
+const initMessaging = firebase.messaging()
---
+++ firebase-cloud-messaging/src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/messaging";
+import "firebase/analytics";
+
+
+const config ={
+    apiKey: "AIzaSyDQOtJLOTNnD0p03dbNPyLKttoPhISUoqM",
+    authDomain: "fir-cloud-messaging-379af.firebaseapp.com",
+    databaseURL: "https://fir-cloud-messaging-379af.firebaseio.com",
+    projectId: "fir-cloud-messaging-379af",
+    storageBucket: "fir-cloud-messaging-379af.appspot.com",
+    messagingSenderId: "1081227225213",
+    appId: "1:1081227225213:web:1afe53577d982bbd6b2914",
+    measurementId: "G-1C3M64FG6L"
+
+};
+firebase.initializeApp(config)
+firebase.analytics();
+export default firebase
